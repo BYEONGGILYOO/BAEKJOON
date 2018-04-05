a1cb5c13c831b61f0506e6a7a614fdf9f2f5dc35
bgyoo-surface <byeonggil.u@gmail.com>
2018-04-05 21:03:50 +0900

    10026 debug

---
+++ DFS/10026.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int N = 0;
+int color[100][100] = {0};
+bool visited[100][100] = {0};
+int dir[4][2] = {{0,-1},{0,1},{-1,0},{1,0}};
+int RGB_cnt[4] = {0};
+
+void dfs(int curr_x, int curr_y)
+{
+    visited[curr_x][curr_y] = true;
+    int curr_color = color[curr_x][curr_y];
+    if(RGB_cnt[curr_color] == 0)
+        RGB_cnt[curr_color] = 1;
+
+    for(int i=0; i<4; i++)
+    {
+        int new_x = curr_x + dir[i][0];
+        int new_y = curr_y + dir[i][1];
+
+        if(new_x >= 0 && new_y >= 0 && new_x < N && new_y < N)
+        {
+            if(!visited[curr_x][curr_y] && color[new_x][new_y] == curr_color)
+            {
+                dfs(new_x, new_y);
+                RGB_cnt[color[new_x][new_y]]++;
+            }
+        }
+    }
+}
+int main(int argc, char** argv)
+{
+    scanf("%d", &N);
+
+    for(int i=0; i<N; i++)
+    {
+        char* tmp = new char[N+1];
+        scanf("%s", tmp);
+        for(int j=0; j<N; j++)
+        {
+            if(tmp[j] == 'R')
+                color[i][j] = 1;
+            else if(tmp[j] == 'G')
+                color[i][j] = 2;
+            else if(tmp[j] == 'B')
+                color[i][j] = 3;
+            else
+                printf("ERROR\n");
+        }
+        delete[] tmp;
+    }
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<N; j++)
+            if(!visited[i][j])
+                dfs(i,j);
+
+    printf("R: %d, G: %d, B: %d\n", RGB_cnt[1], RGB_cnt[2], RGB_cnt[3]);
+
+}
