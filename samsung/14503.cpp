571b05b8540c728dfb26f0799a98494ca334a0b5
YOO-ALIENWARE\BYEONGGIL <byeonggil.u@gmail.com>
2018-04-13 00:18:10 +0900

    solved the problem 14503

--- samsung/14503.cpp
+++ samsung/14503.cpp
@@ -11,12 +11,11 @@ bool input[50][50];
 bool visited[50][50];
 
 // dir def : 0 top, 1 left, 2 bottom, 3 right
-int cnt = 0;
+int cnt = 1;
 
 void dfs(int curr_r, int curr_c, int curr_dir)
 {
     visited[curr_r][curr_c] = true;
-    bool flag = false;
 	
     int next_dir = curr_dir;
     for(int i=0; i<4; i++)
@@ -30,33 +29,30 @@ void dfs(int curr_r, int curr_c, int curr_dir)
         {
             if(!visited[next_r][next_c] && !input[next_r][next_c])
             {
+				/*if(next_dir == 0)
+					printf("ก่\n");
+				else if(next_dir == 1)
+					printf("ก็\n");
+				else if(next_dir == 2)
+					printf("ก้\n");
+				else
+					printf("กๆ\n");*/
                 cnt++;
                 dfs(next_r, next_c, next_dir);
-                flag = true;
-                break;
+				return;
             }
         }
     }
-	// if no way to vacuum, 
-    if(!flag)
-    {
-        next_dir = curr_dir;
-		int next_r = curr_r;
-		int next_c = curr_c;
-        while(1)
-        {
-            next_r = next_r + back[next_dir][0];
-            next_c = next_c + back[next_dir][1];
+	//
+    next_dir = curr_dir;
+
+    int next_r = curr_r + back[next_dir][0];
+    int next_c = curr_c + back[next_dir][1];
             
-            if(next_r >= 0 && next_c >= 0 && next_r < rows && next_c < cols)
-            {
-				if (!input[next_r][next_c])
-					dfs(next_r, next_c, next_dir);
-                else
-                    return;
-            }
-        }
-    }
+    if(next_r >= 0 && next_c >= 0 && next_r < rows && next_c < cols)
+		if (!input[next_r][next_c])
+			dfs(next_r, next_c, next_dir);      
+	return;
 }
 
 int main()
@@ -65,6 +61,10 @@ int main()
 
     int curr_r, curr_c, curr_dir;
     scanf("%d%d%d", &curr_r, &curr_c, &curr_dir);
+	if (curr_dir == 1)
+		curr_dir = 3;
+	else if (curr_dir == 3)
+		curr_dir = 1;
 
     for(int y=0; y<rows; y++)
         for(int x=0; x<cols; x++)
