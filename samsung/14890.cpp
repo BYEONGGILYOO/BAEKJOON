2bb96f35dfbbc6279fff5c8685396f6d2a192623
YOO-SURFACE\BYEONGGIL <byeonggil.u@gmail.com>
2018-04-14 16:37:44 +0900

    solve the problem 14890

--- samsung/14890.cpp
+++ samsung/14890.cpp
@@ -1,104 +1,183 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <vector>
 
 using namespace std;
 
 int N, L;
 int input[100][100];
-int visited[100][100];
-
-//void dfs(int curr_y, int curr_x)
-//{
-//    visited[curr_y][curr_x] = true;
-//    int curr_height = input[curr_y][curr_x];
-//
-//    for(int i=0; i<4; i++)
-//    {
-//        int next_y = curr_y + dir[i][0];
-//        int next_x = curr_x + dir[i][0];
-//
-//        if(next_y >= 0 && next_x >= 0 && next_y < N && next_x < N)
-//        {
-//            int next_height = input[next_y][next_x];
-//            if(curr_height == next_height)
-//                dfs(next_y, next_x);
-//            else if(abs(curr_height - next_height) == 1)
-//            {
-//
-//            }
-//        }
-//    }
-//
-//}
+bool slope[100][100];
 
 int main()
 {
-    scanf("%d%d", &N, &L);
+	scanf("%d%d", &N, &L);
 
-    for(int y=0; y<N; y++)
-        for(int x=0; x<N; x++)
-            scanf("%d", &input[y][x]);
-
-	int result = 0;
 	for (int y = 0; y < N; y++)
-    {
-		int cnt = 0, cnt2 = 0;
+		for (int x = 0; x < N; x++)
+			scanf("%d", &input[y][x]);
+
+	memset(slope, 0, sizeof slope);
+
+	int results = 0;
+	// row-wise search
+	for (int y = 0; y < N; y++)
+	{
+		int step_cnt = 0;
+		int ladder_cnt = 0;
 		for (int x = 0; x < N - 1; x++)
 		{
 			int curr = input[y][x];
 			int next = input[y][x + 1];
 
-			if (curr == next) // 같은경우 경사로 카운트
-				cnt++;
-			else if (curr - next == -1)
+			if (curr == next)
+				ladder_cnt++;
+			else if (abs(curr - next) == 1) // if you need a ladder,
 			{
-				if (cnt < (L - 1))
-					break;
-				else
-					cnt = 0;
-			}
-			else if (curr - next == 1)
-			{
-				cnt = 0;
+				if (curr > next) // down slope
+				{
+					for (int i = 0; i < L; i++)
+					{
+						int next_x = x + 1 + i;
+						if (next_x >= N)
+						{
+							step_cnt = -1;
+							break;
+						}
+
+						if (i != 0 && input[y][next_x] != next)
+						{
+							step_cnt = -1;
+							break;
+						}
+
+						if (slope[y][next_x]) // if already there is a ladder,
+						{
+							step_cnt = -1;
+							break;
+						}
+
+						slope[y][next_x] = true;
+					}
+				}
+				else // up slope
+				{
+					if (ladder_cnt >= (L - 1))
+					{
+						ladder_cnt = 0;
+						for (int i = 0; i < L; i++)
+						{
+							int prev_x = x - i;
+							if (prev_x < 0)
+							{
+								step_cnt = -1;
+								break;
+							}
+
+							if (i != 0 && input[y][prev_x] != curr)
+							{
+								step_cnt = -1;
+								break;
+							}
+
+							if (slope[y][x - i])
+							{
+								step_cnt = -1;
+								break;
+							}
+
+							slope[y][x - i] = true;
+						}
+					}
+					else
+					{
+						step_cnt = -1;
+						break;
+					}
+				}
 			}
 			else
+			{
+				step_cnt = -1;
 				break;
-			cnt2++;
+			}
+			//step_cnt++;
 		}
-		if (cnt2 == N-1)
-			result++;
-		
-    }
+		if (step_cnt != -1)
+			results++;
+	}
+
+	memset(slope, 0, sizeof slope);
+	// col-wise search
 	for (int y = 0; y < N; y++)
 	{
-		int cnt = 0, cnt2 = 0;
+		int step_cnt = 0;
+ 		int ladder_cnt = 0;
 		for (int x = 0; x < N - 1; x++)
 		{
 			int curr = input[x][y];
 			int next = input[x + 1][y];
 
 			if (curr == next)
-				cnt++;
-			else if (abs(curr - next) == 1)
+				ladder_cnt++;
+			else if (abs(curr - next) == 1) // if you need a ladder,
 			{
-				if (cnt < (L - 1))
-					break;
-				else
-					cnt = 0;
+				if (curr > next) // down slope
+				{
+					for (int i = 0; i < L; i++)
+					{
+						int next_x = x + 1 + i;
+						if (next_x >= N) // if array overflow
+						{
+							step_cnt = -1;
+							break;
+						}
+
+						if (i != 0 && input[next_x][y] != next)
+						{
+							step_cnt = -1;
+							break;
+						}
+
+						if (slope[next_x][y]) // if already there is a ladder,
+						{
+							step_cnt = -1;
+							break;
+						}
+
+						slope[next_x][y] = true;
+					}
+				}
+				else // up slope
+				{
+					if (ladder_cnt >= (L - 1))
+					{
+						ladder_cnt = 0;
+						for (int i = 0; i < L; i++)
+						{
+							if (slope[x - i][y])
+							{
+								step_cnt = -1;
+								break;
+							}
+							slope[x - i][y] = true;
+						}
+					}
+					else
+					{
+						step_cnt = -1;
+						break;
+					}
+				}
 			}
 			else
+			{
+				step_cnt = -1;
 				break;
-			cnt2++;
+			}
 		}
-		if (cnt2 == N - 1)
-			result++;
-
+		if (step_cnt != -1)
+			results++;
 	}
 
-	printf("%d\n", result);
-
-	system("pause");
-	return 0;
+	printf("%d\n", results);
 }
