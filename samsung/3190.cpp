b0ec048b999a4eb141482f771a4dd6f1f2b72f95
YOO-ALIENWARE\BYEONGGIL <byeonggil.u@gmail.com>
2018-04-14 19:37:13 +0900

    solve the problem 3190

--- samsung/3190.cpp
+++ samsung/3190.cpp
@@ -39,7 +39,7 @@ int main()
 	}
 	snake_points.push_back(make_pair(0, 0));
 
-	int processing_time = 0;
+	int processing_time = 1;
 	int curr_dir = 0;
 	while (1)
 	{
@@ -49,33 +49,40 @@ int main()
 		if (next_point.first >= N || next_point.second >= N ||
 			next_point.first < 0 || next_point.second < 0)
 			break;
+
 		// if next point is snake
 		vector<pair<int, int>>::iterator snake_itr = snake_points.begin();
 		for (; snake_itr != snake_points.end(); ++snake_itr)
 		{
 			if (snake_itr->first == next_point.first
 				&& snake_itr->second == next_point.second)
-				break;
+			{
+				printf("%d\n", processing_time);
+				return 0;
+			}
 		}
-
 		// eatting apple,
+ 		bool eatApple = false;
 		vector<pair<int, int>>::iterator apple_itr = apple.begin();
 		for (; apple_itr != apple.end(); ++apple_itr)
 		{
 			if (apple_itr->first == next_point.first
 				&& apple_itr->second == next_point.second)
 			{
-				snake_size++;
-				snake_points.push_back(next_point);
-				apple.erase(apple_itr);
+				eatApple = true;
 				break;
 			}
-			else
-			{
-				snake_points.erase(snake_points.begin());
-				snake_points.push_back(next_point);
-				break;
-			}
+		}
+		if (!eatApple)
+		{
+			snake_points.erase(snake_points.begin());
+			snake_points.push_back(next_point);
+		}
+		else
+		{
+			snake_size++;
+			snake_points.push_back(next_point);
+			apple.erase(apple_itr);
 		}
 
 		// next_direction
